3adee2f1a60a6564d3ba9e69ea44b2fd1966e3b4
paul <duffeer83@gmail.com>
2020-07-04 02:07:42 +1000

    Added an x-wing

--- Assets/CameraOrbit.cs
+++ Assets/CameraOrbit.cs
@@ -4,16 +4,83 @@ using UnityEngine;
 
 public class CameraOrbit : MonoBehaviour
 {
+    public float OrbitSensitivity = 10f;
+    public float OrbitDampening = 7f;
+    public Vector2 PitchClamp = new Vector2(-75f, 89f);
+
+    public float ZoomSensitivity = 2f;
+    public float ZoomDampening = 7f;
+    public Vector2 ZoomClamp = new Vector2(1.5f, 50f);
+
+    public float CameraRadius = 1f;
+
+    private float pitch = 0.0f;
+    private float yaw = 0.0f;
+    private float targetZoom = 15.0f; 
+    private float zoom = 15.0f;
+    private float zoomVelocity = 0.0f;
+
+    private static int groundMask = 1 << 8;
+
+    private void Update()
+    {
+        Rotate();
+        Zoom();
+    }
+
+    private void Rotate()
+    {
+        // calculate the new position (if right click)
+        if (Input.GetMouseButton(1))
+        {
+            yaw += Input.GetAxis("Mouse X") * OrbitSensitivity;
+
+            pitch -= Input.GetAxis("Mouse Y") * OrbitSensitivity;
+            pitch = Mathf.Clamp(pitch, PitchClamp.x, PitchClamp.y);
+        }
+
+        // complete the move
+        var qt = Quaternion.Euler(pitch, yaw, 0);
+        transform.parent.rotation = Quaternion.Lerp(transform.parent.rotation, qt, Time.deltaTime * OrbitDampening);
+    }
+
+    private void Zoom()
+    {
+        // calculate the new zoom
+        if (Input.GetAxis("Mouse ScrollWheel") != 0f)
+        {
+            var zoomAmount = Input.GetAxis("Mouse ScrollWheel") * ZoomSensitivity;
+            zoomAmount *= (targetZoom * 0.3f);   // allows further away from object to increase zoom
+            targetZoom += zoomAmount * -1f;
+
+            targetZoom = Mathf.Clamp(targetZoom, ZoomClamp.x, ZoomClamp.y);
+        }
+
+        // determine clipping & damnpen scroll
+        var ray = new Ray(transform.parent.position, -transform.parent.forward);
+        if (Physics.Raycast(ray, out var hit, targetZoom, groundMask))
+            zoom = Mathf.SmoothDamp(zoom, hit.distance - CameraRadius, ref zoomVelocity, Time.deltaTime * ZoomDampening);
+        else
+            zoom = Mathf.SmoothDamp(zoom, targetZoom, ref zoomVelocity, Time.deltaTime * ZoomDampening);
+
+        // complete the zoom
+        transform.localPosition = Vector3.back * zoom;
+    }
+
+    /*
+  
+    // INITIAL VARIANT:
+
     private Transform _camera;
     private Transform _pivot;
 
     private Vector3 _localRotation;
     private float _cameraDistance = 15f;    // the same distance as the camera is offest from the pivot
 
-    public float MouseSensitivity = 4f;
+    public float MouseSensitivity = 10f;
     public float ScrollSensitivity = 2f;
-    public float OrbitDampening = 10f;
-    public float ScrollDampening = 6f;
+    public float OrbitDampening = 7f;
+    public float ScrollDampening = 7f;
 
     public bool CameraDisabled = false;
 
@@ -62,4 +129,5 @@ public class CameraOrbit : MonoBehaviour
             _camera.localPosition = new Vector3(0f, 0f, Mathf.Lerp(_camera.localPosition.z, _cameraDistance * -1f, Time.deltaTime * ScrollDampening));
         
     }
+    */
 }
---
+++ Assets/Collision.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collision : MonoBehaviour
+{
+    private void OnCollisionEnter(UnityEngine.Collision collision)
+    {
+        foreach(var contact in collision.contacts)
+        {
+            Debug.Log("Collision: " + contact);
+        }
+    }
+}
