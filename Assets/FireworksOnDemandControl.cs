a23ea2102194b6dd6e547f7820d42bde41e9cb39
paul <duffeer83@gmail.com>
2020-07-05 00:39:07 +1000

    dynamic fireworks

---
+++ Assets/FireworksOnDemandControl.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireworksOnDemandControl : MonoBehaviour
+{
+    void Start()
+    {
+        // todo: get some details and plug into our vfx
+         
+        // schedule our destruction once explosion compeltely over
+        Destroy(gameObject, 10f);
+    }
+
+}
---
+++ Assets/SpawnFireworks.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnFireworks : MonoBehaviour
+{
+    public GameObject Fireworks;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // click?
+        if (!Input.GetMouseButtonDown(0))
+            return;
+
+        // look for a hit
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (!Physics.Raycast(ray, out var hit))
+            return;
+
+        // spawn!
+        Instantiate(Fireworks, hit.point, Quaternion.identity, transform);
+    }
+}
