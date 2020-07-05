d438c1098dea856beb4ce5ef18d9c23c9b019213
paul <duffeer83@gmail.com>
2020-07-05 18:26:18 +1000

    Explosions!

--- Assets/FireworksOnDemandControl.cs
+++ Assets/FireworksOnDemandControl.cs
@@ -1,15 +1,48 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class FireworksOnDemandControl : MonoBehaviour
 {
+    public AudioClip[] Launch;
+    public AudioClip[] Explosion;
+
+    private AudioSource launchSource;
+    private AudioSource explosionSource;
+
     void Start()
     {
-        // todo: get some details and plug into our vfx
-         
+        // todo: would generally be nice to also have a 'sound travel' delay to both launch and explosion...
+
+        // find a launch sound, create and fire immediately
+        if(Launch.Length > 0)
+        {
+            var launchClip = Launch[Random.Range(0, Launch.Length)];
+            launchSource = CreateAudio(launchClip);
+
+            launchSource.Play();
+        }
+
+        // find an explosion sound, create and fire delayed
+        if (Explosion.Length > 0)
+        {
+            var explosionClip = Explosion[Random.Range(0, Launch.Length)];
+            explosionSource = CreateAudio(explosionClip);
+
+            //1.1 is halfway between 1.0 and 1.2 rocket particle lifespans... eventually would be nice to control this by parameter (calc random, set vfx and play delayed here...)
+            explosionSource.PlayDelayed(1.1f);  
+        }
+
         // schedule our destruction once explosion compeltely over
         Destroy(gameObject, 10f);
     }
 
+    private AudioSource CreateAudio(AudioClip clip)
+    {
+        var source = gameObject.AddComponent<AudioSource>();
+        source.clip = clip;
+        source.spatialBlend = 1f;  // to enable sound fall off from camera
+        source.minDistance = 10f;  // values to effect falloff (logarithmic) curve
+        source.maxDistance = 200f;
+
+        return source;
+    }
 }
---
+++ Assets/GameLife.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameLife : MonoBehaviour
+{
+    public float ThemeMusicStartDelay = 3f;
+
+    private void Start()
+    {
+        // kick off background music
+        var themeMusic = gameObject.GetComponent<AudioSource>();
+        themeMusic?.PlayDelayed(ThemeMusicStartDelay);
+    }
+
+    void Update()
+    {
+        // handle exiting
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
+    }
+}
